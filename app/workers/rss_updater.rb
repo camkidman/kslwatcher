ffe60bad71ee3565bce843753a71ae05f1094b71
Cameron Kidman <cameronjkidman@gmail.com>
2015-05-30 10:50:06 -0600

    Fix mailer stuff, should actually deliver now

--- app/workers/rss_updater.rb
+++ app/workers/rss_updater.rb
@@ -6,7 +6,7 @@ class RssUpdater
     all_terms.each do |term|
       updated_feed = Feedjira::Feed.update(YAML::load(term.feed.serialized_feedjira))
       unless updated_feed.new_entries.blank? && !term.expired
-        updated_feed.new_entries.each {|new_entry| NotificationMailer.new_post(term.user, term.term, new_entry.url).deliver_now }
+        updated_feed.new_entries.each {|new_entry| NotificationMailer.new_post(term.user, term.term, new_entry.url).deliver }
         term.feed.update_attribute :serialized_feedjira, updated_feed
       end
     end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'www.kslnotifications.com' }
+  config.action_mailer.smtp_settings = {
       :address => "smtp.sendgrid.net",
       :port => 587,
       :domain => "kslnotifications.com",
