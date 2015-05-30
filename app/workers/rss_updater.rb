56a04b4c32434b505680a4f2fdfc2228c3494625
Cameron Kidman <cameronjkidman@gmail.com>
2015-05-30 11:19:00 -0600

    change procfile a tiny bit and fix the rss updater so it clears out the new entries every time a feed is updated

--- app/workers/rss_updater.rb
+++ app/workers/rss_updater.rb
@@ -7,6 +7,7 @@ class RssUpdater
       updated_feed = Feedjira::Feed.update(YAML::load(term.feed.serialized_feedjira))
       unless updated_feed.new_entries.blank? && !term.expired
         updated_feed.new_entries.each {|new_entry| NotificationMailer.new_post(term.user, term.term, new_entry.url).deliver }
+        updated_feed.new_entries = []
         term.feed.update_attribute :serialized_feedjira, YAML::dump(updated_feed)
       end
     end
